cfe6403a47021f94c8c2f1af7ab5637ff3f680e3
Shawn <desh0wiz@gmail.com>
2025-06-19 18:39:57 -0700

    Boost stuff

--- Assets/Scripts/Interactables/BoostTIle.cs
+++ Assets/Scripts/Interactables/BoostTIle.cs
@@ -4,10 +4,13 @@ public class BoostTIle : InteractableTile
 {
     [SerializeField]
     private float _boostAmount = 0.5f;
+    [SerializeField]
+    private GameEvent _boostedEvent = null;
     
     public override void Interact()
     {
         GameManager.Instance.Player.Boost(_boostAmount); // Add VFX for speed boost like climbing lines
+        _boostedEvent.Raise();
         gameObject.SetActive(false);
     }
 
